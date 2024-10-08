79c72dcafcb3b09bdaf30e6d72bcfda0209c0938
Robbie <robbiemacinnes@outlook.com>
2024-10-08 20:28:31 +0100

    preprocess data script

--- src/dataset.py
+++ src/dataset.py
@@ -8,14 +8,15 @@ file_path = "../data/external/aclImdb"
 labels = {"pos": 1, "neg": 0}
 pbar = pyprind.ProgBar(50000, stream=sys.stdout)
 
-df = pd.DataFrame()
+data = []  # Collect data in a list first
 for s in ("test", "train"):
     for l in ("pos", "neg"):
         path = os.path.join(file_path, s, l)
         for file in sorted(os.listdir(path)):
             with open(os.path.join(path, file), "r", encoding="utf-8") as infile:
                 txt = infile.read()
-            df = df.append([[txt, labels[1]]], ignore_index=True)
+            data.append([txt, labels[l]])
             pbar.update()
 
-df.columns = ["review", "sentiment"]
+# Convert the list into a DataFrame
+df = pd.DataFrame(data, columns=["review", "sentiment"])
